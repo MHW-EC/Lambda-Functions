9f79d81e25be50ead2db0ab62116620516c9d568
jcoboss <josuealexandercs15@gmail.com>
2022-01-15 17:13:23 -0500

    [Reader] Test mongoose example

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Generador, Reader } = require('@enmanuel_mag/mhwlib');
 const { getResponse } = require('./utils');
-const util = require('util');
+const mongoose = require('mongoose');
 
 exports.generate = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
@@ -61,52 +61,89 @@ exports.generate = (event, context, callback) => {
   return getResponse({ statusCode: 200, body: horarios }, callback); */
 };
 
-exports.read = async (event, context, callback) => {
-  //context.callbackWaitsForEmptyEventLoop = false;
-  console.log('EVENT:', event);
-  console.log('CONTEXT:', context);
+// exports.read = async (event, context, callback) => {
+//   //context.callbackWaitsForEmptyEventLoop = false;
+//   console.log('EVENT:', event);
+//   console.log('CONTEXT:', context);
 
-  if (event.httpMethod.toUpperCase() === 'OPTIONS') {
-    return getResponse({ statusCode: 204 }, callback);
-  }
+//   if (event.httpMethod.toUpperCase() === 'OPTIONS') {
+//     return getResponse({ statusCode: 204 }, callback);
+//   }
 
-  const {
-    body
-  } = event;
-  console.log('BODY: ', body);
+//   const {
+//     body
+//   } = event;
+//   console.log('BODY: ', body);
 
-  if (!Object.keys(body).length) {
-    return getResponse(
-      {
-        statusCode: 400,
-        body: {
-          error: 'No body provided'
-        },
-      },
-      callback
-    );
-  }
-  return Reader.getResourceDataByCb(body, (err, response) => {
-    if (err){
-      console.log(err);
-      return getResponse(
-        {
-          statusCode: 500,
-          body: {
-            err
-          },
-        },
-        callback
-      );
-    };
-    console.log(response);
-    return getResponse(
-      {
-        statusCode: 200,
-        body: response,
-      },
-      callback
-    );
-  })
+//   if (!Object.keys(body).length) {
+//     return getResponse(
+//       {
+//         statusCode: 400,
+//         body: {
+//           error: 'No body provided'
+//         },
+//       },
+//       callback
+//     );
+//   }
+//   return Reader.getResourceDataByCb(body, (err, response) => {
+//     if (err){
+//       console.log(err);
+//       return getResponse(
+//         {
+//           statusCode: 500,
+//           body: {
+//             err
+//           },
+//         },
+//         callback
+//       );
+//     };
+//     console.log(response);
+//     return getResponse(
+//       {
+//         statusCode: 200,
+//         body: response,
+//       },
+//       callback
+//     );
+//   })
  
+// };
+
+
+let conn = null;
+
+const uri = 'mongodb+srv://josue:lospajaroscantanmientraslospecesnadan@heroku-28zqn5s2.htt13.mongodb.net/heroku_28zqn5s2?retryWrites=true&w=majority';
+
+exports.read = async function(event, context) {
+  // Make sure to add this so you can re-use `conn` between function calls.
+  // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  // Because `conn` is in the global scope, Lambda may retain it between
+  // function calls thanks to `callbackWaitsForEmptyEventLoop`.
+  // This means your Lambda function doesn't have to go through the
+  // potentially expensive process of connecting to MongoDB every time.
+  if (conn == null) {
+    conn = mongoose.createConnection(uri, {
+      // and tell the MongoDB driver to not wait more than 5 seconds
+      // before erroring out if it isn't connected
+      serverSelectionTimeoutMS: 5000
+    });
+    
+    // `await`ing connection after assigning to the `conn` variable
+    // to avoid multiple function calls creating new connections
+    await conn;
+    conn.model('Carrera', new mongoose.Schema({nombre: {
+      type: String,
+    }}));
+  }
+
+  const M = conn.model('Carrera');
+
+  const doc = await M.find();
+  console.log(doc);
+
+  return doc;
 };
